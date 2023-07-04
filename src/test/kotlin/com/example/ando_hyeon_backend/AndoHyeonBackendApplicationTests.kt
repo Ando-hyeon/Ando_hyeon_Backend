6d190324bc868fac01d1d83bee092f8afe454d08
jinu <jinwoo794533@gmail.com>
2023-07-04 12:22:34 +0900

    init

---
+++ src/test/kotlin/com/example/ando_hyeon_backend/AndoHyeonBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ando_hyeon_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AndoHyeonBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
